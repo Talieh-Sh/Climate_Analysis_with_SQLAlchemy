46adf10097be136850646ae7a657f9f58dfdedfa
Talieh Sh <talieh.sh.mail@gmail.com>
2023-11-08 21:31:07 +1100

    adding home route

---
+++ SurfsUp/app.py
@@ -0,0 +1,16 @@
+
+# 1. import Flask
+from flask import Flask
+# 2. Create an app, being sure to pass __name__
+app = Flask(__name__)
+# 3. Define what to do when a user hits the index route
+@app.route("/")
+def hello_world():
+    return "hi"
+
+
+    
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
