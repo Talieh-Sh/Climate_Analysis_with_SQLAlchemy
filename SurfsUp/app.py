3158dba07d0a0197bc9d6ea2f357cccc234cab98
Talieh Sh <talieh.sh.mail@gmail.com>
2023-11-08 21:47:14 +1100

    adding sql alchemy libraries to the code

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,3 +1,11 @@
+# Importing related libraries and modules:
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+
 
 # 1. import Flask
 from flask import Flask
@@ -7,15 +15,40 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return (
-        f"Available routes:<br/>"
+        f"<h1>Available routes:</h1>"
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
         f"/api/v1.0/<start> and /api/v1.0/<start> <br/>"
     )
 
+@app.route("/api/v1.0/precipitation")
+#################################################   
+#SQLAlchemy
 
-    
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///../Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(autoload_with=engine)
+
+# Save references to each table
+Measurement=Base.classes.measurement
+Station=Base.classes.station
+
+# Create our session (link) from Python to the DB
+session=Session(engine)
+
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
