6ffe76bd013fadf3c041b7c806181055d601e7c5
Talieh Sh <talieh.sh.mail@gmail.com>
2023-11-08 21:32:13 +1100

    updating flask

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -6,7 +6,13 @@ app = Flask(__name__)
 # 3. Define what to do when a user hits the index route
 @app.route("/")
 def hello_world():
-    return "hi"
+    return (
+        f"Available routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start> and /api/v1.0/<start> <br/>"
+    )
 
 
     
